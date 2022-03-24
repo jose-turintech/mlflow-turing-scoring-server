7f382586da5a745055f544192f4cb8eeacfbb59e
José Ángel Rey Liñares <jose@turintech.ai>
2022-03-24 17:18:21 +0100

    chore: new tests
    new tests

--- src/mlflow_turing_scoring_server/services/nginx_process_service.py
+++ src/mlflow_turing_scoring_server/services/nginx_process_service.py
@@ -13,10 +13,11 @@ class NginxProcessService:
         self.reload_nginx_process()
 
     def reload_nginx_process(self):
-        nginx_conf = resource_filename(
-            mlflow_turing_scoring_server.__name__, "scoring_server/nginx.conf"
-        )
+        # nginx_conf = resource_filename(
+        #    mlflow_turing_scoring_server.__name__, "scoring_server/nginx.conf"
+        #)
+        nginx_conf = "/miniconda/lib/python3.9/site-packages/mlflow/models/container/scoring_server/nginx.conf"
         print("Location of the new nginx conf file:")
         print(nginx_conf)
-
+        Popen(["sed", "-i", "'s/invocations/invocations|turing/g'", "/", nginx_conf])
         Popen(["nginx", "-s", "reload", "-c", nginx_conf, "-t"])
