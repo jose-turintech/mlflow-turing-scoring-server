8570b98316d268c9ff71824349ab640d384ee661
José Ángel Rey Liñares <jose@turintech.ai>
2022-03-24 13:14:28 +0100

    chore: new tests
    new tests

--- src/mlflow_turing_scoring_server/scoring_server/wsgi.py
+++ src/mlflow_turing_scoring_server/scoring_server/wsgi.py
@@ -1,6 +1,11 @@
 import os
 from mlflow_turing_scoring_server import scoring_server
+from mlflow_turing_scoring_server.services.nginx_process_service import NginxProcessService
 from mlflow.pyfunc import load_model
 
-print("Here we go jarey second attemp.")
+print("Starting Turing custom scoring server.")
+print("Managing nginx processes:")
+service: NginxProcessService = NginxProcessService()
+service.manage_custom_nginx()
+
 app = scoring_server.init(load_model(os.environ[scoring_server._SERVER_MODEL_PATH]))
