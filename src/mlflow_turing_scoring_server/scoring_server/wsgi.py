e1e6de71ab6210a7c00de8ef16e9c898316c163c
José Ángel Rey Liñares <jose@turintech.ai>
2022-03-24 15:51:00 +0100

    chore: new tests
    new tests

--- src/mlflow_turing_scoring_server/scoring_server/wsgi.py
+++ src/mlflow_turing_scoring_server/scoring_server/wsgi.py
@@ -3,9 +3,12 @@ from mlflow_turing_scoring_server import scoring_server
 from mlflow_turing_scoring_server.services.nginx_process_service import NginxProcessService
 from mlflow.pyfunc import load_model
 
-print("Starting Turing custom scoring server.")
-print("Managing nginx processes:")
-service: NginxProcessService = NginxProcessService()
-service.manage_custom_nginx()
+try
+    print("Starting Turing custom scoring server.")
+    print("Managing nginx processes:")
+    service: NginxProcessService = NginxProcessService()
+    service.manage_custom_nginx()
+except BaseException:
+    print("Some error happened, coninuing")
 
 app = scoring_server.init(load_model(os.environ[scoring_server._SERVER_MODEL_PATH]))
